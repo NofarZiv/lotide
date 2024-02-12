f0c5fd79439d267a43a8f5694bcae693ee9adcbe
NofarZiv <nofarziv13@gmail.com>
2024-02-12 17:14:45 -0330

    changed the tail function tests to be tested by Mocha and Chai.

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,8 +1,18 @@
 const tail = require('../tail');
+const assert = require('chai').assert;
 
+describe("#tail", () => {
+  it("returns ['Lighthouse', 'Labs'] for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(['Hello', 'Lighthouse', 'Labs']), ['Lighthouse', 'Labs']);
+  });
 
+  it("returns [] for [1]", () => {
+    assert.deepEqual(tail([1]), []);
+  });
 
+  it("returns [] for []", () => {
+    assert.deepEqual(tail([]), []);
+  });
 
-console.log(tail(["Hello", "Lighthouse", "Labs"]));
-console.log(tail([1]));
-console.log(tail([]));
+});
+
