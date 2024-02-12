855b5ddbc72730a6b43edb273f32edd157e88b45
NofarZiv <nofarziv13@gmail.com>
2024-02-12 13:29:38 -0330

    added a module export and seperated the test code.

--- eqArrays.js
+++ eqArrays.js
@@ -10,17 +10,5 @@ const eqArrays = function(arr1, arr2) {
 };
 
 
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    return `✅✅✅ Assertion Passed: ${actual} === ${expected}`;
-  }
-  return `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`;
 
-};
-
-
-
-console.log(assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true));
-console.log(assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false));
-console.log(assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true));
-console.log(assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false));
+module.exports = eqArrays;
