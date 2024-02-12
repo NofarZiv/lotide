631d1503ce3a4c1193cabb422f908aa64cf60a97
NofarZiv <nofarziv13@gmail.com>
2024-02-12 12:46:00 -0330

    refactored the test code that was duplicated, and replaced that function with a require call instead.

--- assertEqual.js
+++ assertEqual.js
@@ -2,10 +2,10 @@ const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`)
-  };
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
 
 };
 
-console.log(assertEqual("Bootcamp", "Bootcamp"));
-console.log(assertEqual(1, 2));
+
+module.exports = assertEqual;
