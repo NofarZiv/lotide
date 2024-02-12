08da172015001809c7bf8c5ff9706d1545da8b29
NofarZiv <nofarziv13@gmail.com>
2024-02-12 17:18:53 -0330

    changed the middle function tests to be tested by Mocha and Chai.

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,7 +1,15 @@
 const middle = require('../middle');
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 
 
 
-let result = middle([1, 2, 3, 4, 5, 6]);
-assertArraysEqual(result, [3, 4]);
+describe("#middle", () => {
+  it("returns [3, 4] for [1, 2, 3, 4, 5, 6]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+  });
+
+});
+
+
+
+
