da9af4a8c143f101e4287b7c0666c762e583750c
NofarZiv <nofarziv13@gmail.com>
2024-02-12 16:36:45 -0330

    added a module export and seperated the test code.

--- middle.js
+++ middle.js
@@ -1,23 +1,3 @@
-const eqArrays = function(arr1, arr2) {
-  for (let i = 0; i < arr1.length; i++) {
-    for (let x = 0; x < arr2.length; x++) {
-      if (i === x && arr1[i] !== arr2[x]) {
-        return false;
-      }
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function(actual, expected) {
-  if (eqArrays(actual, expected)) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-
 const middle  = function(arr) {
   let newArr = [];
   let length = arr.length
@@ -36,5 +16,6 @@ const middle  = function(arr) {
 };
 
 
-let result = middle([1, 2, 3, 4, 5, 6]);
-assertArraysEqual(result, [3, 4]);
+
+
+module.exports = middle;
