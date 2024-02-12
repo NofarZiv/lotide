53d2d8f502a029276948e771bb65bd15a61f4fca
NofarZiv <nofarziv13@gmail.com>
2024-02-12 12:47:47 -0330

    refactored the test code that was duplicated before, and replaced that function with a require call instead.

--- head.js
+++ head.js
@@ -3,14 +3,6 @@ const head = function(arr) {
   return first;
 };
 
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    return `✅✅✅ Assertion Passed: ${actual} === ${expected}`;
-  }
-  return `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`;
 
-};
 
-console.log(assertEqual(head([5,6,7]), 5));
-console.log(assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"));
-console.log(assertEqual(head([]), undefined));
+module.exports = head;
