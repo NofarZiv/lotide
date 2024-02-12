c136d9abdd8b39e182926263393409091ceef2e5
NofarZiv <nofarziv13@gmail.com>
2024-02-12 11:06:35 -0330

    fixed the function so it will print the assertions and not only return it.

--- assertEqual.js
+++ assertEqual.js
@@ -1,8 +1,9 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    return `✅✅✅ Assertion Passed: ${actual} === ${expected}`;
-  }
-  return `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`;
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`)
+  };
 
 };
 
